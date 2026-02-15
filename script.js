85820b978daa19c93e539278c84137c78cae7307
haise314 <yudsdanielpenaranda@gmail.com>
2026-02-16 05:14:13 +0800

    Add funciton to the new grid button
    Added functionality to the new grid button

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Generate a grid generator
-// Set the grid size
+// TODO: Generate a grid generator
 
 // If I just wrap the divs, then make it so that
 // the flex is set based on gridSize
@@ -10,6 +9,7 @@ let gridSize = 16;
 const gridContainer = document.querySelector("#grid-container");
 
 function generateGrid (gridSize){
+    console.log("Gridsize:" + gridSize);
 
     for (let i = 0; i < gridSize; i++){
         for (let j = 0; j < gridSize; j++){
@@ -19,9 +19,33 @@ function generateGrid (gridSize){
 
             let gridWidth = 100 / gridSize;
             tile.style.flex = `1 1 ${gridWidth}%`;
+            console.log("j count: " + j);
         }
+        console.log("i count: " + i);
     }
 }
 
 generateGrid(gridSize);
+
+// TODO: add logic to the button then connect it
+
+const newGridBtn = document.querySelector("#new-grid-btn");
+
+newGridBtn.addEventListener("click", () => {
+    // remove all children
+    while (gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    gridSize = prompt("Enter new grid size.");
+    generateGrid(gridSize);
+})
+
+
+
+// TODO: Add event listeners to the entire grid 
+//       probably mouse events then set colors
+// TODO: Have the pixel change color have trailing effect
+// TODO: Randomize the trailing effect and darken the 
+//       previous ones by 10% per trail 
 
