9a350a2244a0d2979605dcc8df7eae966b1e3ba3
haise314 <yudsdanielpenaranda@gmail.com>
2026-02-16 04:44:18 +0800

    Add generateGrid function and tile styling
    Added grid generation logic that dynamically scales its children

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 // Generate a grid generator
 // Set the grid size
+
+// If I just wrap the divs, then make it so that
+// the flex is set based on gridSize
+// all is peachy
+
+let gridSize = 16;
+
+const gridContainer = document.querySelector("#grid-container");
+
+function generateGrid (gridSize){
+
+    for (let i = 0; i < gridSize; i++){
+        for (let j = 0; j < gridSize; j++){
+            const tile = document.createElement("div");
+            tile.className = "tile";
+            gridContainer.appendChild(tile);
+
+            let gridWidth = 100 / gridSize;
+            tile.style.flex = `1 1 ${gridWidth}%`;
+        }
+    }
+}
+
+generateGrid(gridSize);
+
